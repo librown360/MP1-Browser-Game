069f91d539737febb97bcf284bdfd04967764663
Lisa Brown <lisaann493@verizon.net>
2022-01-15 17:29:04 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,48 @@
+// Create layout for board, scorecard, player options
+//  1 - Add div for each component
+//       - the board
+//       - player options
+//       - game pieces 
+//       - scorecard
+//  2 - Add id attribute for each div with name for each component
+
+
+// Allow user to decide whether to play against computer or another person
+//  1 - Show two options on the screen
+//  2 - Code for two players first
+//  3 - Code for one player against computer
+//  4 - Check for efficiency (Is code DRY?)
+
+
+// Allow user to pick game piece (X or O)
+//  1 - Prompt player 1 to choose game piece
+//  2 - Assign chosen game piece to player 1 and unchosen to player 2 (or sys)
+
+
+// Prompt user to pick a square
+//  1 - Prompt player 1 to pick a square
+//  2 - When square is chosen display chosen game piece in the square
+//  3 - Disable chosen squares from being picked
+//  4 - Prompt player 2 to pick a square
+//  5 - Display other game piece in chosen square
+//  6 - Continue to disable chosen squares
+//  NOTE: If playing computer #4 & #5 will be different logic
+
+
+// After first user picks a square prompt second user to pick a square
+// Or if second player is the computer pick strategicly after a second or 2
+//  1 - See above steps ;)
+
+
+// When either player has marked 3 squares in a row (vertically, horizontally,
+// or diagonally) display a message indicating who won
+//  1 - Display a message when either player wins
+//  2 - The message should indicate who won
+//  3 - Maybe include some animated confetti or celebratory animation
+//  4 - When there is no winner (game is a scratch) indicate so in message
+
+
+// If time permits keep track of overall score locally
+//  1 - Need to look up how to do this from past lessons
+//  2 - 
+
